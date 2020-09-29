f6a1b565743563afc92fd7cacf1f57a85059686a
Jeffrey Lu <yumyumyum@yumyumythership.fios-router.home>
2020-09-28 22:26:48 -0400

    tests cors again for post request

--- server.js
+++ server.js
@@ -10,7 +10,7 @@ const PORT = process.env.PORT || 5000;
 
 app.use(express.json({ extended: false }));
 app.use(cookieParser());
-// app.use(cors());
+app.use(cors());
 
 // app.options('/api/login', cors());
 
