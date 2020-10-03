9046cacf1654ca934b4dcd577bc7b20dc32238d6
Robert Luo <robertxluo@gmail.com>
2020-10-03 13:00:40 -0500

    Clean register controller

--- auth/register.js
+++ auth/register.js
@@ -4,23 +4,34 @@ const jwt = require('jsonwebtoken');
 const register = async (req, res, next) => {
   try {
     const { username, password } = req.body;
-    if (!username || !password) return next('Invalid input.');
+
+    if (!username || !password) return next({ message: `The email and password field is required.` });
+
     const results = await User.findAll({ where: { username } });
+
     if (results.length) {
-      res.locals.newUser = { err: `An account with this email already exists` };
+      res.locals.newUser = { statusCode: 409, message: `An account with this email already exists.` };
       return next();
     }
+
     const user = await User.create({ username, password });
-    if (!user) return next({ err: 'There was an error. Please try again later.' });
+
+    if (!user) return next({ message: 'There was an error. Please try again later.' });
+
     const payload = { user_id: user.user_id };
     const secret = process.env.SECRET;
     const token = jwt.sign(payload, secret, { expiresIn: '1h' });
-    if (process.env.NODE_ENV === 'production') res.cookie('token', token, { httpOnly: true, sameSite: 'none', secure: true });
-    else res.cookie('token', token, { httpOnly: true });
+
+    if (process.env.NODE_ENV === 'production') {
+      res.cookie('token', token, { httpOnly: true, sameSite: 'none', secure: true });
+    } else {
+      res.cookie('token', token, { httpOnly: true });
+    }
+
     res.locals.newUser = { user_id: user.user_id };
-    next();
+    return next();
   } catch (err) {
-    next(err);
+    return next({ statusCode: 400, message: err.message });
   }
 };
 
