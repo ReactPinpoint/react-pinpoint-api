81b740153afaf80ba0cfa13ea4700441c252d29b
Robert Luo <robertxluo@gmail.com>
2020-10-03 13:18:46 -0500

    Clean up register

--- auth/register.js
+++ auth/register.js
@@ -5,18 +5,21 @@ const register = async (req, res, next) => {
   try {
     const { username, password } = req.body;
 
-    if (!username || !password) return next({ message: `The email and password field is required.` });
+    if (!username || !password) {
+      return next({ message: `The email and password field is required.` });
+    }
 
     const results = await User.findAll({ where: { username } });
 
     if (results.length) {
-      res.locals.newUser = { statusCode: 409, message: `An account with this email already exists.` };
-      return next();
+      return next({ statusCode: 409, message: `An account with this email already exists.` });
     }
 
     const user = await User.create({ username, password });
 
-    if (!user) return next({ message: 'There was an error. Please try again later.' });
+    if (!user) {
+      return next({ message: 'There was an error. Please try again later.' });
+    }
 
     const payload = { user_id: user.user_id };
     const secret = process.env.SECRET;
