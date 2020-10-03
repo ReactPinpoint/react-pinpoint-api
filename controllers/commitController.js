df5a0acaa25545886473b33625e2b8d8375edcc6
Tai N <ndhuutai1@gmail.com>
2020-10-03 11:14:41 -0700

    added children_ids

--- controllers/commitController.js
+++ controllers/commitController.js
@@ -5,17 +5,20 @@ const commitController = {};
 commitController.getCommit = async (req, res, next) => {
   try {
     const { project_id } = req.params;
-    if (!project_id) return next({ err: 'Invalid project.'});
-    const project = await Project.findByPk(project_id, { include: { all: true, nested: true } });
+    if (!project_id) return next({ err: 'Invalid project.' });
+    const project = await Project.findByPk(project_id, {
+      include: { all: true, nested: true },
+    });
     // console.log(project)
-    if (!project) return next({ err: 'An error occured. Please try again later.'});
+    if (!project)
+      return next({ err: 'An error occured. Please try again later.' });
     const { changes } = project;
     res.locals.changes = changes;
     next();
   } catch (err) {
     next(err);
   }
-}
+};
 
 commitController.addCommit = async (req, res, next) => {
   try {
@@ -23,34 +26,49 @@ commitController.addCommit = async (req, res, next) => {
     const { changes } = req.body;
 
     // request validation
-    if (!project_id) return next({ err: 'Invalid project.'});
+    if (!project_id) return next({ err: 'Invalid project.' });
     if (!changes) return next({ err: 'No changes sent.' });
-    if (!changes.length) return next({ err: 'Changes length is zero.'});
+    if (!changes.length) return next({ err: 'Changes length is zero.' });
     // console.log(changes)
 
     // loop through changes array and validate data
     for (let i = 0; i < changes.length; i += 1) {
       const commit = changes[i];
-      console.log(commit)
-      if (typeof commit !== 'object') return next({ err: 'Commit data is invalid.'})
-      const { 
-        component_id, 
-        component_name, 
-        self_base_duration, 
-        parent_component_id, 
-        component_state, 
-        sibling_component_id } = commit;
-      if (!Object.hasOwnProperty.call(commit, 'component_id') || !component_id) return next({ err: 'component_id is missing.'});
-      if (!Object.hasOwnProperty.call(commit, 'component_name') || !component_name) return next({ err: 'component_name is missing.'});
-      if (!Object.hasOwnProperty.call(commit, 'self_base_duration') || !self_base_duration) return next({ err: 'self_base_duration is missing.'});
-      if (!Object.hasOwnProperty.call(commit, 'parent_component_id') || !parent_component_id) return next({ err: 'parent_component_id is missing.'});
-      if (!Object.hasOwnProperty.call(commit, 'component_state') || !component_state) return next({ err: 'component_state is missing.'});
-      if (!Object.hasOwnProperty.call(commit, 'sibling_component_id') || !sibling_component_id) return next({ err: 'sibling_component_id is missing.'});
+      console.log(commit);
+      if (typeof commit !== 'object')
+        return next({ err: 'Commit data is invalid.' });
+      const {
+        component_id,
+        component_name,
+        self_base_duration,
+        parent_component_id,
+        component_state,
+        sibling_component_id,
+      } = commit;
+      // if (!Object.hasOwnProperty.call(commit, 'component_id') || !component_id) return next({ err: 'component_id is missing.'});
+      // if (
+      //   !Object.hasOwnProperty.call(commit, 'component_name') ||
+      //   !component_name
+      // )
+      //   return next({ err: 'component_name is missing.' });
+      // if (
+      //   !Object.hasOwnProperty.call(commit, 'self_base_duration') ||
+      //   !self_base_duration
+      // )
+      //   return next({ err: 'self_base_duration is missing.' });
+      // // if (!Object.hasOwnProperty.call(commit, 'parent_component_id') || !parent_component_id) return next({ err: 'parent_component_id is missing.'});
+      // if (
+      //   !Object.hasOwnProperty.call(commit, 'component_state') ||
+      //   !component_state
+      // )
+      //   return next({ err: 'component_state is missing.' });
+      // if (!Object.hasOwnProperty.call(commit, 'sibling_component_id') || !sibling_component_id) return next({ err: 'sibling_component_id is missing.'});
     }
 
     // if request data is validated then create the change and associate with project_id
     const change = await Change.create({});
-    if (!change) return next({ err: "An error occured. Please try again later."});
+    if (!change)
+      return next({ err: 'An error occured. Please try again later.' });
     change.setProject(project_id);
     const { change_id } = change;
     // console.log('change_id ->',  change_id)
@@ -59,20 +77,23 @@ commitController.addCommit = async (req, res, next) => {
     // if change sucessfully created, create the commits and associate them with the change_id
     const commit_ids = [];
     for (let j = 0; j < changes.length; j += 1) {
-      const { 
-        component_id, 
-        component_name, 
-        self_base_duration, 
-        parent_component_id, 
-        component_state, 
-        sibling_component_id } = changes[j];
-      const commit = await Commit.create({
-        component_id, 
-        component_name, 
+      const {
+        component_id,
+        component_name,
         self_base_duration,
-        parent_component_id, 
-        component_state, 
-        sibling_component_id
+        parent_component_id,
+        component_state,
+        sibling_component_id,
+        children_ids,
+      } = changes[j];
+      const commit = await Commit.create({
+        component_id,
+        component_name,
+        self_base_duration,
+        parent_component_id,
+        component_state,
+        sibling_component_id,
+        children_ids,
       });
       commit.setChange(change_id);
       const { commit_id } = commit;
@@ -85,6 +106,6 @@ commitController.addCommit = async (req, res, next) => {
   } catch (err) {
     next(err);
   }
-}
+};
 
 module.exports = commitController;
