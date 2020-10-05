1234ff73de27384b2cb753faf2af229e3955cbda
Robert Luo <robertxluo@gmail.com>
2020-10-04 19:48:50 -0500

    Add status codes

--- server.js
+++ server.js
@@ -42,19 +42,19 @@ app.use('/api/project', isAuthorized, projectRouter);
 app.use('/api/commit', commitRouter);
 
 app.get('/api/auth', isAuthorized, (req, res) => {
-  res.json(res.locals.user_id);
+  res.status(200).json(res.locals.user_id);
 });
 
 app.post('/api/register', register, (req, res) => {
-  res.json(res.locals.newUser);
+  res.status(201).res.json(res.locals.newUser);
 });
 
 app.post('/api/login', login, (req, res) => {
-  res.json(res.locals.loggedIn);
+  res.status(200).res.json(res.locals.loggedIn);
 });
 
 app.get('/api/logout', logout, (req, res) => {
-  res.json(res.locals.loggedOut);
+  res.status(200).res.json(res.locals.loggedOut);
 });
 
 app.get('/', isAuthorized, (req, res) => {
