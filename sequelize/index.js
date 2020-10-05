fbe44dad071150786561b1e9ab9970c98fb713c9
Robert Luo <robertxluo@gmail.com>
2020-10-05 10:59:47 -0500

    Add color to sequelize logs

--- sequelize/index.js
+++ sequelize/index.js
@@ -3,7 +3,7 @@ const Sequelize = require('sequelize');
 const dbname = process.env.DB_NAME;
 const username = process.env.DB_USERNAME;
 const password = process.env.DB_PASSWORD;
-const dbport = process.env.DB_PORT
+const dbport = process.env.DB_PORT;
 
 const sequelize = new Sequelize(dbname, username, password, {
   host: process.env.DB_URL,
@@ -14,22 +14,21 @@ const sequelize = new Sequelize(dbname, username, password, {
     min: 0,
     acquire: 30000,
     idle: 10000,
-  }
+  },
 });
 
 (async () => {
   try {
     await sequelize.authenticate();
-    console.log('Connected to DB.');
+    console.log('Connected to DB.'.cyan);
     if (process.env.NODE_ENV === 'development') {
-      console.log(sequelize.models)
+      console.log(sequelize.models);
       await sequelize.sync();
-      console.log('All models synced.');
+      console.log('All models synced.'.green.bold);
     }
   } catch (error) {
-    console.log(error);
+    console.log(`${error}`.red);
   }
-}
-)();
+})();
 
 module.exports = sequelize;
