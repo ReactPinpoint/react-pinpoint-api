4f61ae09841455ccad288f7afd0e0e35d2ac0def
Robert Luo <robertxluo@gmail.com>
2020-10-05 11:02:17 -0500

    Update auth code

--- auth/auth.js
+++ auth/auth.js
@@ -2,16 +2,40 @@ const { User } = require('../sequelize/models');
 const jwt = require('jsonwebtoken');
 
 const isAuthorized = async (req, res, next) => {
-  try {
-    const { token } = req.cookies;
+  // Default to no user logged in
+  req.session = null;
+  req.user = null;
 
-    const user_id = await jwt.verify(token, process.env.SECRET, (err, decoded) => {
-      if (err) return undefined;
-      return decoded.user_id;
+  // Helper method to clear a token and invoke the next middleware
+  const clearTokenAndNext = () => {
+    res.clearCookie('token');
+    return next({ statusCode: 401, message: 'You are not authorized to view this page.' });
+  };
+
+  try {
+    // Read the cookie named 'token' and bail out if it doesn't exist
+    const { token } = req.cookies;
+    if (!token) {
+      return clearTokenAndNext();
+    }
+
+    // Verify the validity of the token
+    const user_id = await jwt.verify(token, process.env.SECRET, (err, decodedToken) => {
+      if (err) {
+        return clearTokenAndNext();
+      }
+
+      // Compare the token expiry (in seconds) to the current time (in milliseconds)
+      // Bail out if the token has expired
+      if (decodedToken.exp <= Date.now() / 1000) {
+        return clearTokenAndNext();
+      }
+
+      return decodedToken.user_id;
     });
 
     if (!user_id) {
-      return next({ statusCode: 401, message: 'You are not authorized to view this page.' });
+      return clearTokenAndNext();
     }
 
     const user = await User.findByPk(user_id);
