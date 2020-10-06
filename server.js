1c36efdb79baa1cf3dda8b2dcb5995292856554e
Robert Luo <robertxluo@gmail.com>
2020-10-05 23:14:06 -0500

    Fix variable names to accomodate UI code

--- server.js
+++ server.js
@@ -42,19 +42,19 @@ app.use('/api/project', isAuthorized, projectRouter);
 app.use('/api/commit', commitRouter);
 
 app.get('/api/auth', isAuthorized, (req, res) => {
-  res.status(200).json({ success: true, user: res.locals.user_id });
+  res.status(200).json({ success: true, user_id: res.locals.user_id });
 });
 
 app.post('/api/register', register, (req, res) => {
-  res.status(201).json({ success: true, user: res.locals.newUser });
+  res.status(201).json({ success: true, newUser: res.locals.newUser });
 });
 
 app.post('/api/login', login, (req, res) => {
-  res.status(200).json({ success: true, user: res.locals.loggedIn });
+  res.status(200).json({ success: true, loggedIn: res.locals.loggedIn });
 });
 
 app.get('/api/logout', logout, (req, res) => {
-  res.status(200).json({ success: true, user: res.locals.loggedOut });
+  res.status(200).json({ success: true, loggedOut: res.locals.loggedOut });
 });
 
 app.get('/', isAuthorized, (req, res) => {
